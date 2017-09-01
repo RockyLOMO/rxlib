d49b3c13746c4b87e8e20818086143ba5dbfd2fa
R <rx2@fake.com>
2017-09-01 18:03:44 +0800

    lazy<>

---
+++ src/main/java/org/rx/common/Lazy.java
@@ -0,0 +1,40 @@
+package org.rx.common;
+
+import java.util.function.Supplier;
+
+/**
+ * Created by IntelliJ IDEA. User: za-wangxiaoming Date: 2017/9/1
+ */
+public final class Lazy<T> {
+    private T                 value;
+    private final Supplier<T> supplier;
+
+    public boolean isValueCreated() {
+        return value != null;
+    }
+
+    public T getValue() {
+        if (value == null) {
+            synchronized (supplier) {
+                if (value == null) {
+                    value = supplier.get();
+                }
+            }
+        }
+        return value;
+    }
+
+    public Lazy(Class<T> type) {
+        this(() -> {
+            try {
+                return type.newInstance();
+            } catch (ReflectiveOperationException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+    }
+
+    public Lazy(Supplier<T> supplier) {
+        this.supplier = supplier;
+    }
+}
---
+++ src/main/java/org/rx/socket/PingClient.java
@@ -0,0 +1,88 @@
+package org.rx.socket;
+
+import com.google.common.base.Stopwatch;
+import org.rx.common.Lazy;
+import org.rx.util.App;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static org.rx.common.Contract.require;
+import static org.rx.util.App.isNull;
+import static org.rx.util.App.logError;
+
+/**
+ * Created by IntelliJ IDEA. User: za-wangxiaoming Date: 2017/9/1
+ */
+public final class PingClient {
+    public class Result {
+        private final List<Long> results;
+        private Lazy<Double>     avg;
+        private Lazy<Long>       min, max;
+
+        public List<Long> getResults() {
+            return results;
+        }
+
+        public int getLossCount() {
+            return (int) results.stream().filter(p -> p == null).count();
+        }
+
+        public double getAvg() {
+            return avg.getValue();
+        }
+
+        public long getMin() {
+            return min.getValue();
+        }
+
+        public long getMax() {
+            return max.getValue();
+        }
+
+        private Result(List<Long> results) {
+            this.results = results;
+            avg = new Lazy<>(() -> results.stream().mapToDouble(p -> isNull(p, 0L)).average().getAsDouble());
+            min = new Lazy<>(() -> results.stream().filter(p -> p != null).mapToLong(p -> p).min().orElse(-1));
+            max = new Lazy<>(() -> results.stream().filter(p -> p != null).mapToLong(p -> p).max().orElse(-1));
+        }
+    }
+
+    private Stopwatch watcher;
+
+    public PingClient() {
+        watcher = Stopwatch.createUnstarted();
+    }
+
+    public Result ping(String endpoint) {
+        return ping(App.parseSocketAddress(endpoint));
+    }
+
+    public Result ping(InetSocketAddress sockAddr) {
+        require(sockAddr);
+
+        return new Result(Arrays.stream(new Long[4]).map(p -> {
+            Socket sock = new Socket();
+            try {
+                watcher.start();
+                sock.connect(sockAddr);
+            } catch (IOException ex) {
+                logError(ex, "ping");
+                return null;
+            } finally {
+                watcher.stop();
+                try {
+                    sock.close();
+                } catch (IOException ex) {
+                    logError(ex, "PingClient.ping()");
+                }
+            }
+            return watcher.elapsed(TimeUnit.MILLISECONDS);
+        }).collect(Collectors.toList()));
+    }
+}
