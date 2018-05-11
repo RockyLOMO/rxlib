4af2ad62009c26e4d314e44b640d6b290aad95f6
bloodyCoder <bloodyCoder@gmail.com>
2018-05-11 18:02:51 +0800

    dx

--- src/main/java/org/rx/InvalidOperationException.java
+++ src/main/java/org/rx/InvalidOperationException.java
@@ -1,6 +1,13 @@
 package org.rx;
 
+/**
+ * 根据Exception来读取errorCode.yml的错误信息
+ */
 public class InvalidOperationException extends SystemException {
+    public static SystemException friendly(String format, Object... args) {
+        return new InvalidOperationException(null).setFriendlyMessage(format, args);
+    }
+
     public InvalidOperationException(String format, Object... args) {
         super(String.format(format, args));
     }
@@ -16,5 +23,13 @@ public class InvalidOperationException extends SystemException {
     public <T extends Enum<T>> InvalidOperationException(Throwable e, T errorCode, Object... messageValues) {
         super(e);
         setErrorCode(errorCode, messageValues);
+    }
+
+    public InvalidOperationException(Object[] messageValues, String errorName) {
+        super(messageValues, errorName);
+    }
+
+    public InvalidOperationException(Object[] messageValues, Throwable ex) {
+        super(messageValues, ex);
     }
 }
