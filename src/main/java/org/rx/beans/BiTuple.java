bf96cb5927770006e09838a0ce819257cce89fcc
BloodyR <ilovehaley.kid@gmail.com>
2019-05-25 17:03:35 +0800

    fvvk

--- src/main/java/org/rx/beans/BiTuple.java
+++ src/main/java/org/rx/beans/BiTuple.java
@@ -1,42 +1,20 @@
 package org.rx.beans;
 
-import lombok.EqualsAndHashCode;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@EqualsAndHashCode
+@Data
+@NoArgsConstructor
 public final class BiTuple<T1, T2, T3> {
     public static <T1, T2, T3> BiTuple<T1, T2, T3> of(T1 t1, T2 t2, T3 t3) {
         return new BiTuple<>(t1, t2, t3);
     }
 
-    public final T1 left;
-    public final T2 middle;
-    public final T3 right;
+    public T1 left;
+    public T2 middle;
+    public T3 right;
 
-    public T1 getLeft() {
-        return left;
-    }
-
-    public void setLeft(T1 left) {
-        //this.left = left;
-    }
-
-    public T2 getMiddle() {
-        return middle;
-    }
-
-    public void setMiddle(T2 middle) {
-        //this.middle = middle;
-    }
-
-    public T3 getRight() {
-        return right;
-    }
-
-    public void setRight(T3 right) {
-        //this.right = right;
-    }
-
-    private BiTuple(T1 t1, T2 t2, T3 t3) {
+    public BiTuple(T1 t1, T2 t2, T3 t3) {
         left = t1;
         middle = t2;
         right = t3;
--- src/main/java/org/rx/beans/Tuple.java
+++ src/main/java/org/rx/beans/Tuple.java
@@ -1,33 +1,19 @@
 package org.rx.beans;
 
-import lombok.EqualsAndHashCode;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@EqualsAndHashCode
+@Data
+@NoArgsConstructor
 public final class Tuple<T1, T2> {
     public static <T1, T2> Tuple<T1, T2> of(T1 t1, T2 t2) {
         return new Tuple<>(t1, t2);
     }
 
-    public final T1 left;
-    public final T2 right;
+    public T1 left;
+    public T2 right;
 
-    public T1 getLeft() {
-        return left;
-    }
-
-    public void setLeft(T1 left) {
-        //this.left = left;
-    }
-
-    public T2 getRight() {
-        return right;
-    }
-
-    public void setRight(T2 right) {
-        //this.right = right;
-    }
-
-    private Tuple(T1 t1, T2 t2) {
+    public Tuple(T1 t1, T2 t2) {
         left = t1;
         right = t2;
     }
