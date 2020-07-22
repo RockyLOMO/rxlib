40a1c464dbfecb012d004a78741191303b8e2193
wangxiaoming <1748083+RockyLOMO@users.noreply.github.com>
2020-07-22 10:58:21 +0800

    Update SessionClient.java

--- src/main/java/org/rx/socks/tcp/SessionClient.java
+++ src/main/java/org/rx/socks/tcp/SessionClient.java
@@ -2,9 +2,11 @@ package org.rx.socks.tcp;
 
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelId;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.rx.bean.DateTime;
 
 import java.net.InetSocketAddress;
 
@@ -12,8 +14,10 @@ import java.net.InetSocketAddress;
 public class SessionClient<T> {
     protected final Channel channel;
     @Getter
-    @Setter
-    private String groupId;
+    private final DateTime connectedTime = DateTime.now();
+    @Getter
+    @Setter(AccessLevel.PACKAGE)
+    private volatile String groupId;
     @Getter
     private final T state;
 
