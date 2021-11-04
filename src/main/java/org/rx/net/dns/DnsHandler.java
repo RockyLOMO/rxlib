c81d1ac047ec5bd863ca369e4383e49dd8dbfb07
℞ <ilovehaley.kid@gmail.com>
2021-11-04 11:43:21 +0800

    Update DnsHandler.java

--- src/main/java/org/rx/net/dns/DnsHandler.java
+++ src/main/java/org/rx/net/dns/DnsHandler.java
@@ -111,7 +111,7 @@ public class DnsHandler extends SimpleChannelInboundHandler<DefaultDnsQuery> {
             if (!f.isSuccess()) {
                 log.error("query domain {} fail", domain, f.cause());
 //                ctx.writeAndFlush(DnsMessageUtil.newErrorResponse(query, DnsResponseCode.NXDOMAIN));
-                ctx.writeAndFlush(newResponse(query, question, server.ttl, NQuery.of(InetAddress.getAllByName(domain)).select(InetAddress::getAddress).toArray()));
+                ctx.writeAndFlush(newResponse(query, question, server.ttl));
                 return;
             }
             AddressedEnvelope<DnsResponse, InetSocketAddress> envelope = (AddressedEnvelope<DnsResponse, InetSocketAddress>) f.getNow();
