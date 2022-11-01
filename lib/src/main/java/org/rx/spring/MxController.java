b94ca6f5a3b331d5896ddcdfbc5cd8c38a3c17f1
youfan <115806199+youfanx@users.noreply.github.com>
2022-11-01 22:32:54 +0800

    mxpwd

--- lib/src/main/java/org/rx/spring/MxController.java
+++ lib/src/main/java/org/rx/spring/MxController.java
@@ -1,9 +1,10 @@
 package org.rx.spring;
 
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
 import com.sun.management.HotSpotDiagnosticMXBean;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.rx.core.*;
 import org.rx.core.StringBuilder;
 import org.rx.exception.TraceHandler;
@@ -20,9 +21,11 @@ import java.io.File;
 import java.lang.management.ManagementFactory;
 import java.net.InetAddress;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
+import static org.rx.core.App.fromJson;
+import static org.rx.core.Extends.eq;
 
 @RequiredArgsConstructor
 @RestController
@@ -34,7 +37,11 @@ public class MxController {
     public Map<String, Object> queryTraces(Boolean newest, String level,
                                            Boolean methodOccurMost, String methodNamePrefix,
                                            String metricsName,
-                                           Integer take) {
+                                           Integer take,
+                                           HttpServletRequest request) {
+        if (!check(request)) {
+            return null;
+        }
         Map<String, Object> result = new LinkedHashMap<>(3);
         ExceptionLevel el = null;
         if (!Strings.isBlank(level)) {
@@ -44,7 +51,7 @@ public class MxController {
 
         result.put("methodTraces", Linq.from(TraceHandler.INSTANCE.queryTraces(methodOccurMost, methodNamePrefix, take))
                 .select(p -> {
-                    JSONObject t = App.toJsonObject(p);
+                    Map<String, Object> t = App.toJsonObject(p);
                     t.remove("elapsedMicros");
                     t.put("elapsed", App.formatElapsed(p.getElapsedMicros()));
                     return t;
@@ -54,56 +61,39 @@ public class MxController {
         return result;
     }
 
-    @RequestMapping("setConfig")
-    public RxConfig setConfig(@RequestBody RxConfig config) {
-        return BeanMapper.DEFAULT.map(config, RxConfig.INSTANCE);
-    }
-
-    @RequestMapping("svr")
-    public JSONObject server(HttpServletRequest request) {
-        JSONObject j = new JSONObject(true);
-        j.put("jarFile", App.getJarFile(this));
-        j.put("inputArguments", ManagementFactory.getRuntimeMXBean().getInputArguments());
-        HotSpotDiagnosticMXBean bean = ManagementFactory.getPlatformMXBean(HotSpotDiagnosticMXBean.class);
-        try {
-            bean.setVMOption("UnlockCommercialFeatures", Boolean.TRUE.toString());
-        } catch (Exception e) {
-            j.put("UnlockCommercialFeatures", e.toString());
+    @RequestMapping("health")
+    public Object health(HttpServletRequest request) {
+        if (!check(request)) {
+            return null;
         }
-        j.put("vmOptions", bean.getDiagnosticOptions());
-        j.put("systemProperties", System.getProperties());
-        j.put("cpuThreads", Constants.CPU_THREADS);
-        File root = new File("/");
-        j.put("diskUsableSpace", Bytes.readableByteSize(root.getUsableSpace()));
-        j.put("diskTotalSpace", Bytes.readableByteSize(root.getTotalSpace()));
-        j.put("ntpOffset", Reflects.readField(NtpClock.class, "offset"));
-
-//        j.put("conf", conf);
-        j.put("requestHeaders", Linq.from(Collections.list(request.getHeaderNames()))
-                .select(p -> String.format("%s: %s", p, String.join("; ", Collections.list(request.getHeaders(p))))));
-        j.putAll(queryTraces(null, null, null, null, null, 10));
-        return j;
-    }
-
-    @RequestMapping("setVMOption")
-    public void setVMOption(String k, String v) {
-        HotSpotDiagnosticMXBean bean = ManagementFactory.getPlatformMXBean(HotSpotDiagnosticMXBean.class);
-        bean.setVMOption(k, v);
-    }
-
-    @RequestMapping("shell")
-    public String shell(String shell, String workspace) {
-        StringBuilder echo = new StringBuilder();
-        ShellCommander cmd = new ShellCommander(shell, workspace);
-        cmd.onPrintOut.combine((s, e) -> echo.append(e.toString()));
-        cmd.start().waitFor(60);
-        return echo.toString();
-    }
-
-    @SneakyThrows
-    @RequestMapping("resolveHost")
-    public Object[] resolveHost(String host) {
-        return Linq.from(InetAddress.getAllByName(host)).select(p -> p.getHostAddress()).toArray();
+        try {
+            switch (Integer.parseInt(request.getParameter("method"))) {
+                case 1:
+                    String config = request.getParameter("config");
+                    return BeanMapper.DEFAULT.map(fromJson(config, new TypeReference<RxConfig>() {
+                    }.getType()), RxConfig.INSTANCE);
+                case 2:
+                    String k = request.getParameter("k"),
+                            v = request.getParameter("v");
+                    HotSpotDiagnosticMXBean bean = ManagementFactory.getPlatformMXBean(HotSpotDiagnosticMXBean.class);
+                    bean.setVMOption(k, v);
+                    return "ok";
+                case 3:
+                    String host = request.getParameter("host");
+                    return Linq.from(InetAddress.getAllByName(host)).select(p -> p.getHostAddress()).toArray();
+                case 4:
+                    String a1 = request.getParameter("cmd"),
+                            a2 = request.getParameter("workspace");
+                    StringBuilder echo = new StringBuilder();
+                    ShellCommander cmd = new ShellCommander(a1, a2);
+                    cmd.onPrintOut.combine((s, e) -> echo.append(e.toString()));
+                    cmd.start().waitFor(60);
+                    return echo.toString();
+            }
+            return svrState(request);
+        } catch (Throwable e) {
+            return String.format("%s\n%s", e, ExceptionUtils.getStackTrace(e));
+        }
     }
 
 //    @PostMapping("directOffer")
@@ -122,6 +112,35 @@ public class MxController {
 //            binary.read(out);
 //        }
 //    }
+
+    Map<String, Object> svrState(HttpServletRequest request) {
+        Map<String, Object> j = new LinkedHashMap<>();
+        j.put("jarFile", App.getJarFile(this));
+        j.put("inputArguments", ManagementFactory.getRuntimeMXBean().getInputArguments());
+        HotSpotDiagnosticMXBean bean = ManagementFactory.getPlatformMXBean(HotSpotDiagnosticMXBean.class);
+        try {
+            bean.setVMOption("UnlockCommercialFeatures", Boolean.TRUE.toString());
+        } catch (Exception e) {
+            j.put("UnlockCommercialFeatures", e.toString());
+        }
+        j.put("vmOptions", bean.getDiagnosticOptions());
+        j.put("systemProperties", System.getProperties());
+        j.put("cpuThreads", Constants.CPU_THREADS);
+        File root = new File("/");
+        j.put("diskUsableSpace", Bytes.readableByteSize(root.getUsableSpace()));
+        j.put("diskTotalSpace", Bytes.readableByteSize(root.getTotalSpace()));
+        j.put("ntpOffset", Reflects.readField(NtpClock.class, null, "offset"));
+
+//        j.put("conf", conf);
+        j.put("requestHeaders", Linq.from(Collections.list(request.getHeaderNames()))
+                .select(p -> String.format("%s: %s", p, String.join("; ", Collections.list(request.getHeaders(p))))));
+        j.putAll(queryTraces(null, null, null, null, null, 10, request));
+        return j;
+    }
+
+    boolean check(HttpServletRequest request) {
+        return eq(request.getHeader(RxConfig.ConfigNames.MXPWD), RxConfig.INSTANCE.getMxpwd());
+    }
 
     @SneakyThrows
     @PostConstruct
