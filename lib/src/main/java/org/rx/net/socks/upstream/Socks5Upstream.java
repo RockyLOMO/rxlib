52662c11d9c54273e47332c2acbcd60e92d14505
℞ <ilovehaley.kid@gmail.com>
2022-12-25 11:42:17 +0800

    Update Socks5Upstream.java

--- lib/src/main/java/org/rx/net/socks/upstream/Socks5Upstream.java
+++ lib/src/main/java/org/rx/net/socks/upstream/Socks5Upstream.java
@@ -3,11 +3,14 @@ package org.rx.net.socks.upstream;
 import io.netty.channel.Channel;
 import lombok.NonNull;
 import lombok.SneakyThrows;
+import org.apache.commons.lang3.BooleanUtils;
 import org.rx.codec.CodecUtil;
 import org.rx.core.Arrays;
 import org.rx.core.Cache;
 import org.rx.core.Sys;
+import org.rx.core.Tasks;
 import org.rx.exception.InvalidException;
+import org.rx.exception.TraceHandler;
 import org.rx.net.AuthenticEndpoint;
 import org.rx.net.Sockets;
 import org.rx.net.socks.SocksConfig;
@@ -15,6 +18,9 @@ import org.rx.net.support.SocksSupport;
 import org.rx.net.support.UnresolvedEndpoint;
 import org.rx.net.support.UpstreamSupport;
 import org.rx.util.function.Func;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import static org.rx.core.Tasks.awaitQuietly;
 
@@ -48,10 +54,32 @@ public class Socks5Upstream extends Upstream {
             long hash = CodecUtil.hash64(dstEpStr);
             //change dest first
             destination = new UnresolvedEndpoint(String.format("%s%s", hash, SocksSupport.FAKE_HOST_SUFFIX), Arrays.randomNext(SocksSupport.FAKE_PORT_OBFS));
+//            Cache<Long, Boolean> cache = Cache.getInstance();
+//            if (!cache.containsKey(hash)) {
+//                try {
+//                    Tasks.runAsync(() -> {
+//                        Sys.logCtx(String.format("socks5[%s]", config.getListenPort()), dstEpStr);
+//                        support.fakeEndpoint(hash, dstEpStr);
+//                        return true;
+//                    }).whenCompleteAsync((r, e) -> {
+//                        if (BooleanUtils.isTrue(r)) {
+//                            cache.put(hash, r);
+//                        }
+//                    }).get(SocksSupport.ASYNC_TIMEOUT, TimeUnit.MILLISECONDS);
+//                } catch (Exception e) {
+//                    TraceHandler.INSTANCE.log(e);
+//                }
+//            }
+
             Cache.getOrSet(hash, k -> awaitQuietly(() -> {
-                Sys.logCtx(String.format("socks5[%s]", config.getListenPort()), dstEpStr);
-                support.fakeEndpoint(hash, dstEpStr);
-                return true;
+                try {
+                    Sys.logCtx(String.format("socks5[%s]", config.getListenPort()), dstEpStr);
+                    support.fakeEndpoint(hash, dstEpStr);
+                    return true;
+                } catch (Throwable e) {
+                    TraceHandler.INSTANCE.log("test", e);
+                    return null;
+                }
             }, SocksSupport.ASYNC_TIMEOUT));
         }
 
