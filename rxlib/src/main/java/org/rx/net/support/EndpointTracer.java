f4b218032ba1ea05d3dee3b3798f048d66b95bbe
youfan <115806199+youfanx@users.noreply.github.com>
2023-02-15 09:28:32 +0800

    reduce eptracer

--- rxlib/src/main/java/org/rx/net/support/EndpointTracer.java
+++ rxlib/src/main/java/org/rx/net/support/EndpointTracer.java
@@ -1,8 +1,6 @@
 package org.rx.net.support;
 
 import io.netty.channel.Channel;
-import lombok.EqualsAndHashCode;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.rx.bean.Tuple;
 import org.rx.core.Cache;
@@ -11,30 +9,21 @@ import java.net.SocketAddress;
 
 @Slf4j
 public final class EndpointTracer {
-    @RequiredArgsConstructor
-    @EqualsAndHashCode
-    static class LinkedData {
-        final SocketAddress head;
-//        final List<SocketAddress> nodes = new CopyOnWriteArrayList<>();
-    }
-
-    final Cache<Tuple<String, SocketAddress>, LinkedData> index = Cache.getInstance(Cache.MEMORY_CACHE);
+    final Cache<Tuple<String, SocketAddress>, SocketAddress> index = Cache.getInstance(Cache.MEMORY_CACHE);
 
     Tuple<String, SocketAddress> key(SocketAddress sa) {
         return Tuple.of("EpTracer", sa);
     }
 
     public void link(Channel inbound, Channel outbound) {
-        LinkedData data = index.get(key(outbound.localAddress()), k -> new LinkedData(inbound.remoteAddress()));
-//        data.nodes.addAll(Arrays.toList(inbound.remoteAddress(), inbound.localAddress(), outbound.localAddress(), outbound.remoteAddress()));
-//        index.put(key(outbound.localAddress()), data);
-        log.info("EpTracer link {} <- {} {}", data.head, inbound, outbound);
+        SocketAddress data = index.get(key(outbound.localAddress()), k -> inbound.remoteAddress());
+//        log.info("EpTracer link {} <- {} {}", data.head, inbound, outbound);
     }
 
     public SocketAddress head(Channel channel) {
-        LinkedData data = index.get(key(channel.remoteAddress()));
-        SocketAddress head = data == null ? channel.remoteAddress() : data.head;
-        log.info("EpTracer head {} <- {}", head, channel);
+        SocketAddress data = index.get(key(channel.remoteAddress()));
+        SocketAddress head = data == null ? channel.remoteAddress() : data;
+//        log.info("EpTracer head {} <- {}", head, channel);
         return head;
     }
 }
