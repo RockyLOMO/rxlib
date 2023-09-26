ff08b0a80599493032a97d820bf5748127b523c8
wxm <115806199+youfanx@users.noreply.github.com>
2023-09-26 11:08:42 +0800

    up

--- rxlib/src/main/java/org/rx/net/socks/SocksConfig.java
+++ rxlib/src/main/java/org/rx/net/socks/SocksConfig.java
@@ -23,6 +23,7 @@ public class SocksConfig extends SocketConfig {
     private int trafficShapingInterval = 10000;
     private int readTimeoutSeconds = 60 * 4;
     private int writeTimeoutSeconds;
+    private int udpAssociateMaxLifeSeconds = 60 * 60 * 12;
     private int udpReadTimeoutSeconds = 60 * 20;
     private int udpWriteTimeoutSeconds;
     private boolean enableUdp2raw;
