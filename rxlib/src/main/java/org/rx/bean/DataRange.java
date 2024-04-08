a5faaf45d0c8356f292c45833103fa1bac2a50dc
wxm <115806199+youfanx@users.noreply.github.com>
2024-04-08 18:41:00 +0800

    Update DataRange.java

--- rxlib/src/main/java/org/rx/bean/DataRange.java
+++ rxlib/src/main/java/org/rx/bean/DataRange.java
@@ -3,6 +3,8 @@ package org.rx.bean;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import org.rx.core.Reflects;
+import org.rx.core.Strings;
 
 import java.io.Serializable;
 
@@ -12,6 +14,12 @@ import static org.rx.core.Extends.require;
 @NoArgsConstructor
 public class DataRange<T extends Comparable<T>> implements Serializable {
     private static final long serialVersionUID = 2698228026798507997L;
+
+    public static <T extends Comparable<T>> DataRange<T> of(String expr, Class<T> type) {
+        String[] vals = Strings.split(expr, "-", 2);
+        return new DataRange<>(Reflects.changeType(vals[0], type), Reflects.changeType(vals[1], type));
+    }
+
     public T start;
     public T end;
 
