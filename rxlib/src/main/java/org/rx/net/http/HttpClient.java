0df65f81acd33f9c3f06a3d9a92fffe56c08c36b
wxm <115806199+youfanx@users.noreply.github.com>
2024-12-05 15:14:31 +0800

    Update HttpClient.java

--- rxlib/src/main/java/org/rx/net/http/HttpClient.java
+++ rxlib/src/main/java/org/rx/net/http/HttpClient.java
@@ -343,8 +343,11 @@ public class HttpClient {
 
         for (String pair : pairs) {
             int idx = pair.indexOf(":");
-            String key = idx > 0 ? URLDecoder.decode(pair.substring(0, idx), StandardCharsets.UTF_8.name()) : pair;
-            String value = idx > 0 && pair.length() > idx + 1 ? URLDecoder.decode(pair.substring(idx + 1), StandardCharsets.UTF_8.name()).trim() : Strings.EMPTY;
+            if (idx == -1) {
+                continue;
+            }
+            String key = URLDecoder.decode(pair.substring(0, idx), StandardCharsets.UTF_8.name());
+            String value = pair.length() > idx + 1 ? URLDecoder.decode(pair.substring(idx + 1), StandardCharsets.UTF_8.name()).trim() : Strings.EMPTY;
             map.put(key, value);
         }
         return map;
