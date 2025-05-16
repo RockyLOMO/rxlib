74543f3e657a424dbf574bb569b0e200849481b3
wxm <115806199+youfanx@users.noreply.github.com>
2025-05-16 17:43:26 +0800

    Update GeoLite2.java

--- rxlib/src/main/java/org/rx/net/support/GeoLite2.java
+++ rxlib/src/main/java/org/rx/net/support/GeoLite2.java
@@ -8,11 +8,11 @@ import lombok.Setter;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.rx.core.Sys;
+import org.rx.core.Tasks;
 import org.rx.exception.InvalidException;
 import org.rx.net.Sockets;
 import org.rx.net.dns.DnsClient;
 import org.rx.net.http.HttpClient;
-import org.rx.util.Lazy;
 
 import java.io.File;
 import java.net.InetAddress;
@@ -20,20 +20,9 @@ import java.util.Collections;
 
 @Slf4j
 public class GeoLite2 implements IPSearcher {
-    final Lazy<DatabaseReader> reader;
+    DatabaseReader reader;
     @Setter
     String resolveServer;
-
-    public GeoLite2() {
-        reader = new Lazy<>(() -> {
-            File f = new File("GeoLite2-City.mmdb");
-            if (!f.exists()) {
-                new HttpClient().withTimeoutMillis(10 * 60 * 1000)
-                        .get("https://cloud.f-li.cn:6400/GeoLite2-City.mmdb").toFile(f.getName());
-            }
-            return new DatabaseReader.Builder(f).build();
-        });
-    }
 
     @Override
     public String getPublicIp() {
@@ -76,10 +65,24 @@ public class GeoLite2 implements IPSearcher {
         if (Sockets.isPrivateIp(ip)) {
             return new IPAddress(ip.getHostAddress(), "Private", null, null);
         }
+        if (reader == null) {
+            Tasks.run(this::init);
+            return new IPAddress(ip.getHostAddress(), "Init..", null, null);
+        }
 
-        CityResponse result = reader.getValue().city(ip);
+        CityResponse result = reader.city(ip);
         Country country = result.getCountry();
         City city = result.getCity();
         return new IPAddress(ip.getHostAddress(), country.getName(), country.getIsoCode(), city.getName());
+    }
+
+    @SneakyThrows
+    synchronized void init() {
+        File f = new File("GeoLite2-City.mmdb");
+        if (!f.exists()) {
+            new HttpClient().withTimeoutMillis(10 * 60 * 1000)
+                    .get("https://cloud.f-li.cn:6400/GeoLite2-City.mmdb").toFile(f.getName());
+        }
+        reader = new DatabaseReader.Builder(f).build();
     }
 }
