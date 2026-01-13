7586fc132a52e10ab0092f8a134ca46862b0fc5a
Rocky <115806199@users.noreply.github.com>
2026-01-13 16:06:22 +0800

    up

--- rxlib/src/main/java/org/rx/net/support/GeoIPSearcher.java
+++ rxlib/src/main/java/org/rx/net/support/GeoIPSearcher.java
@@ -52,7 +52,7 @@ public class GeoIPSearcher implements Closeable {
         }
 
         String[] services = resolveServer != null
-                ? new String[]{"https://" + resolveServer + "/getPublicIp", "https://checkip.amazonaws.com", "https://api.seeip.org"}
+                ? new String[]{"https://" + resolveServer + ":8082/getPublicIp", "https://checkip.amazonaws.com", "https://api.seeip.org"}
                 : new String[]{"https://checkip.amazonaws.com", "https://api.seeip.org"};
         HttpClient client = new HttpClient().withTimeoutMillis(5000);
         for (String service : services) {
