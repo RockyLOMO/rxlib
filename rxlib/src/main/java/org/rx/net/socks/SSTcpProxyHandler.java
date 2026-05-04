c03f646ee922655b715d87dac15146a5db83791a
Rocky <1748083+RockyLOMO@users.noreply.github.com>
2026-05-04 09:42:19 -0700

    fix(socks): restore Shadowsocks TCP route behavior [tests=CipherCodecTest,SSUdpProxyHandlerTest,ShadowsocksServerIntegrationTest,SocksUdpRelayHandlerTest,TimeoutVerificationTest,UdpValidationTest]

--- rxlib/src/main/java/org/rx/net/socks/SSTcpProxyHandler.java
+++ rxlib/src/main/java/org/rx/net/socks/SSTcpProxyHandler.java
@@ -1,7 +1,6 @@
 package org.rx.net.socks;
 
 import io.netty.channel.*;
-import io.netty.util.ReferenceCountUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.rx.net.BackpressureHandler;
 import org.rx.net.Sockets;
@@ -9,7 +8,7 @@ import org.rx.net.socks.upstream.Upstream;
 import org.rx.net.support.EndpointTracer;
 import org.rx.net.support.UnresolvedEndpoint;
 
-import java.net.SocketAddress;
+import java.net.InetSocketAddress;
 
 @Slf4j
 @ChannelHandler.Sharable
@@ -22,49 +21,19 @@ public class SSTcpProxyHandler extends ChannelInboundHandlerAdapter {
         ShadowsocksServer server = Sockets.getAttr(inbound, ShadowsocksConfig.SVR);
         boolean debug = server.config.isDebug();
         UnresolvedEndpoint dstEp = inbound.attr(ShadowsocksConfig.REMOTE_DEST).get();
-        if (dstEp == null) {
-            log.warn("SS TCP missing destination, close {}", Sockets.getOriginRemoteAddress(inbound));
-            ReferenceCountUtil.release(msg);
-            inbound.close();
-            return;
-        }
 
         SocksContext e = SocksContext.getCtx(Sockets.getOriginRemoteAddress(inbound), dstEp);
-        try {
-            server.publishEvent(server.onTcpRoute, e);
-        } catch (Throwable routeError) {
-            log.warn("SS TCP route error for {}", dstEp, routeError);
-            ReferenceCountUtil.release(msg);
-            inbound.close();
-            return;
-        }
-
+        server.publishEvent(server.onTcpRoute, e);
         Upstream upstream = e.getUpstream();
-        if (upstream == null || upstream.getDestination() == null) {
-            log.warn("SS TCP route upstream invalid for {} from {}", dstEp, Sockets.getOriginRemoteAddress(inbound));
-            ReferenceCountUtil.release(msg);
-            inbound.close();
-            return;
-        }
-
         UnresolvedEndpoint upDstEp = upstream.getDestination();
-        SocketAddress connectAddress;
-        try {
-            connectAddress = upDstEp.socketAddress();
-        } catch (Throwable routeError) {
-            log.warn("SS TCP route destination invalid for {} from {}", upDstEp, Sockets.getOriginRemoteAddress(inbound), routeError);
-            ReferenceCountUtil.release(msg);
-            inbound.close();
-            return;
-        }
 
         ChannelFuture outboundFuture = Sockets.bootstrap(inbound.eventLoop(), upstream.getConfig(), upstream.connectAddressHint(), outbound -> {
             upstream.initChannel(outbound);
             inbound.pipeline().addLast(SocksTcpFrontendRelayHandler.DEFAULT);
-        }).connect(connectAddress).addListener((ChannelFutureListener) f -> {
+        }).connect(upDstEp.socketAddress()).addListener((ChannelFutureListener) f -> {
             if (!f.isSuccess()) {
                 log.warn("SS TCP connect to backend {}[{}] fail", upDstEp, dstEp, f.cause());
-                inbound.close();
+                Sockets.closeOnFlushed(inbound);
                 return;
             }
             if (debug) {
