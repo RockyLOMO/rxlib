fdd3ee9f201ad6a1f68470e2b977ff558b329ed2
Rocky <f@users.noreply.github.com>
2026-05-12 10:58:56 +0800

    up

--- rxlib/src/main/java/org/rx/net/socks/encryption/CryptoAeadBase.java
+++
@@ -1,4 +0,0 @@
-/*
- * Legacy Bouncy Castle AEAD base is disabled after Shadowsocks support was
- * narrowed to aes-256-gcm backed by JDK JCE/NIO ByteBuf crypto.
- */
--- rxlib/src/main/java/org/rx/net/socks/encryption/impl/AesGcmCrypto.java
+++
@@ -1,4 +0,0 @@
-/*
- * Legacy Bouncy Castle AES-GCM implementation is disabled. Use
- * Aes256GcmByteBufCrypto for the only supported Shadowsocks AEAD cipher.
- */
